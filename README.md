c6d3db7fedc5f21e9232cff1044717dbc197a262
meeeeemu <126832077+meeeeemu@users.noreply.github.com>
2023-07-28 02:35:06 +0100

    update 0.0.2

---
+++ README.md
@@ -0,0 +1,29 @@
+# bored incremental
+
+# https://meeeeemu.github.io/bored-incremental/
+
+## [v0.0.2] - we have the rebirthing
+
+### added:
+  - added a draggable rebirth window. once you get 1e6 points you can reset for a +0.1x multiplier on all point gain
+
+### changed:
+  - improved save loading
+  - changed some window positions, looks alot cleaner now
+
+### bugs fixed:
+  - rebirth window would allow for you to spam the button and get infinite multiplier lol
+
+### things to add:
+  - ~~rebirthing (global multiplier for all points)~~
+  - rebirth rebirthing (global multiplier for the global multiplier)
+  - automatic upgrading
+  - automatic rebirthing
+  - settings page to customize some values
+  - pretty much remake the whole website structure
+    
+my first incremental game made with break eternity
+
+it is not fun so have fun
+
+i am not very good at math :)
---
+++ main.js
@@ -0,0 +1,147 @@
+import Decimal from "break_eternity.js";
+
+if(localStorage.getItem("boredincrementaltest")){
+    var parseddata = JSON.parse(localStorage.getItem("boredincrementaltest"))
+}
+
+console.log(parseddata)
+
+if(parseddata){
+    var data = {
+        pointValue: new Decimal(parseddata.pointValue),
+        incrementValue: new Decimal(parseddata.incrementValue),
+        upgradeCost: new Decimal(parseddata.upgradeCost),
+        intervalCost: new Decimal(parseddata.intervalCost),
+        intervalTime: parseddata.intervalTime,
+        decimalPlaceCount: parseddata.decimalPlaceCount,
+        rebirthMultiplier: new Decimal(parseddata.rebirthMultiplier),
+        rebirthCounts: new Decimal(parseddata.rebirthCounts),
+        rebirthPrice: new Decimal(parseddata.rebirthPrice)
+    }
+} else {
+    var data = {
+        pointValue: new Decimal(0),
+        incrementValue: new Decimal(0.001),
+        upgradeCost: new Decimal(0.01),
+        intervalCost: new Decimal(0.001),
+        intervalTime: 500,
+        decimalPlaceCount: 6,
+        rebirthMultiplier: new Decimal(1),
+        rebirthCounts: new Decimal(0),
+        rebirthPrice: new Decimal(1e6)
+    }
+}
+
+
+document.getElementById("currentPointIncrement").innerHTML = data.incrementValue;
+
+document.getElementById("upgradeCost").innerHTML = data.upgradeCost;
+
+document.getElementById("currentPointInterval").innerHTML = data.intervalTime;
+
+function save() {
+    localStorage.setItem("boredincrementaltest", JSON.stringify(data));
+  }
+
+function deleteSave() {
+    localStorage.removeItem("boredincrementaltest");
+}
+
+function handleVisibilityChange() {
+    if (document.visibilityState === "hidden") {
+      save();
+    }
+}
+
+document.addEventListener("visibilitychange", handleVisibilityChange);
+
+document.getElementById("deleteSave").addEventListener('click', function () {
+    const confirmed = window.confirm("are you shore you wanna delete it");
+    if (confirmed) {
+      alert("save is gonezo");
+      deleteSave();
+      location.reload();
+    }
+});
+
+document.getElementById("saveButton").addEventListener('click', function () {
+    save();
+});
+
+document.getElementById("deleteSave").addEventListener('click', function () {
+    document.removeEventListener("visibilitychange", handleVisibilityChange);
+});
+
+document.getElementById("saveButton").addEventListener('click', function () {
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+});
+
+function mainIncrement(){
+    data.pointValue = (data.pointValue.add(data.incrementValue.multiply(data.rebirthMultiplier)));
+    document.getElementById("pointValue").innerHTML = data.pointValue.toPrecision(data.decimalPlaceCount);
+    if(document.getElementById("pointCostInterval")){
+        document.getElementById("pointCostInterval").innerHTML = ((data.pointValue.multiply(16))/50).toPrecision(data.decimalPlaceCount);
+    }
+    if(data.intervalTime==1){
+        document.getElementById("upgradeInterval").innerText = "he's going fast (max)";
+        document.getElementById("upgradeInterval").style = "cursor: not-allowed; pointer-events: none;";
+    }
+    document.getElementById('rebirthPotential').innerHTML = data.rebirthMultiplier.toPrecision(data.decimalPlaceCount)
+    document.getElementById('rebirthPrice').innerHTML = data.rebirthPrice.toExponential()
+}
+
+document.getElementById('rebirthButton').addEventListener('click', function(){
+    console.log(data.pointValue.cmp(data.rebirthPrice));
+    if(data.pointValue.cmp(data.rebirthPrice)==1){
+        data.rebirthMultiplier = data.rebirthMultiplier.add(0.1);
+        document.getElementById('rebirthPotential').innerHTML = data.rebirthMultiplier.toPrecision(data.decimalPlaceCount)
+        data.pointValue = new Decimal(0);
+        data.incrementValue = new Decimal(0.001);
+        data.upgradeCost = new Decimal(0.01);
+        data.intervalCost = new Decimal(0.001);
+        data.intervalTime = 500;
+        data.decimalPlaceCount = 6;
+        data.rebirthCounts = data.rebirthCounts.add(1);
+        data.rebirthPrice = data.rebirthPrice.multiply(data.rebirthCounts.div(0.5))
+        document.getElementById('currentPointIncrement').innerHTML = data.incrementValue.multiply(data.rebirthMultiplier).toPrecision(data.decimalPlaceCount);
+        document.getElementById('upgradeCost').innerHTML = data.upgradeCost.toPrecision(data.decimalPlaceCount);
+        document.getElementById('currentPointInterval').innerHTML = data.intervalTime;
+        document.getElementById("upgradeInterval").innerHTML = "convert <div id='pointCostInterval'>0</div> to lower interval value";
+        document.getElementById("upgradeInterval").style = "";
+    }
+})
+
+
+
+document.getElementById("upgradeIncrement").addEventListener('click', function(){
+    console.log(data.pointValue.cmp(data.upgradeCost));
+    if(data.pointValue.cmp(data.upgradeCost)==1){
+        data.pointValue = data.pointValue.subtract(data.upgradeCost);
+        data.upgradeCost = (data.upgradeCost.multiply(3.141)).multiply(data.rebirthMultiplier.multiply(0.7));
+        data.incrementValue = data.incrementValue.multiply(2.482345);
+        document.getElementById("upgradeCost").innerHTML = data.upgradeCost.toPrecision(data.decimalPlaceCount);
+        document.getElementById("currentPointIncrement").innerHTML = data.incrementValue.toPrecision(data.decimalPlaceCount);
+    }
+})
+
+document.getElementById("upgradeInterval").addEventListener('click', function(){
+    console.log(data.pointValue.cmp(data.upgradeCost));
+    if(data.pointValue){
+        data.intervalTime = data.intervalTime - (data.pointValue.multiply(16))/50;
+        if(data.intervalTime<=0){
+            data.intervalTime = 1;
+            document.getElementById("upgradeInterval").innerText = "he's going fast (max)";
+            document.getElementById("upgradeInterval").style = "cursor: not-allowed; pointer-events: none;";
+        } else {
+            data.pointValue = data.pointValue.subtract(data.pointValue);
+        }
+        document.getElementById("currentPointInterval").innerHTML = data.intervalTime;
+    }
+})
+
+function mainGameLoop(){
+    mainIncrement();
+    let intervalTime = data.intervalTime;
+    setTimeout(mainGameLoop, intervalTime);
+}
+mainGameLoop()
